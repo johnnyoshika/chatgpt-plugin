0c366303ea79f171bac3022eddb6ab7b793656d5
Johnny Oshika <johnnyoshika@gmail.com>
2023-04-05 16:29:52 -0700

    List todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { initializeDb } from '@/config/database';
+
 export default async function Home() {
-  return <div>Hello!</div>;
+  const db = await initializeDb();
+  const todos = await db.all<
+    {
+      id: number;
+      username: string;
+      todo: string;
+    }[]
+  >('SELECT * FROM todos');
+
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.id} {todo.username}: {todo.todo}
+        </li>
+      ))}
+    </ul>
+  );
 }
