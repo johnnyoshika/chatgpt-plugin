4fb815f5f0b8f5d43a9d4376d377ebf04776d545
Johnny Oshika <johnnyoshika@gmail.com>
2023-04-06 09:07:39 -0700

    Enable CORS everywhere

--- next.config.js
+++ next.config.js
@@ -3,6 +3,32 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        // Enable CORS everywhere - needed for ChatGPT to access /.well-known/ai-plugin.json and /openapi.yaml
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+      {
+        // Specify that we allow all headers in CORS requests (especially important in preflight OPTIONS request).
+        // We don't need to specify Access-Control-Allow-Methods and Access-Control-Allow-Origin as they'll be specified automatically.
+        source: '/api/todos/:username*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
